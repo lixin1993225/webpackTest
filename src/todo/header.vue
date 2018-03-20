ef3a943e9a7607002fa1b726b088606d49b8eab6
liZn <1063383308@qq.com>
2018-03-20 18:19:36 +0800

    aa

---
+++ src/todo/header.vue
@@ -0,0 +1,19 @@
+<template>
+	<header class="main_header">
+		<h1>JUST DO IT</h1>
+	</header>
+</template>
+<script type="text/javascript">
+	export default {
+
+	}
+</script>
+<style type="text/css" lang="stylus" scoped>
+	.main_header
+		text-align:center
+		h1
+			font-size 50px
+			fot-weight 100
+			margin 20px
+			color red
+</style>
---
+++ src/todo/todo.vue
@@ -0,0 +1,29 @@
+<template>
+	<section>
+		<input 
+		type="text"
+		name=""
+		class="addinf"
+		/>
+	</section>
+</template>
+<script type="text/javascript">
+	export default{
+		data(){
+			return {
+
+			}
+		}
+	}
+</script>
+<style type="text/css" lang="stylus" scoped>
+	section
+		width 500px
+		height 100px
+		margin 0 auto
+		.addinf
+			width 500px
+			height 50px
+			border 1px solid #ccc
+			outline none
+</style>
